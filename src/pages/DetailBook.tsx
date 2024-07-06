df5bae3e95f75fe2672273b7fbc9de56852ef69c
ekorahy <ekorahy@gmail.com>
2024-07-06 20:09:28 +0700

    styling: change styling for detail book page

--- src/pages/DetailBook.tsx
+++ src/pages/DetailBook.tsx
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getBookById } from "../data/local/books";
 import { Book, ReadingStatus } from "../types";
+import { MdArrowBack } from "react-icons/md";
 
 export default function DetailBook() {
   const { id = "" } = useParams<{ id: string }>();
@@ -31,89 +32,100 @@ export default function DetailBook() {
 
   return (
     <section>
-      <img
-        src={book.cover_image}
-        className="mx-auto w-80 rounded-md"
-        alt={book.title}
-      />
-      <table className="mx-auto mt-4 table-auto border-collapse border border-gray-200">
-        <tbody>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Title
-            </td>
-            <td className="border border-gray-200 px-4 py-2">{book.title}</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Author
-            </td>
-            <td className="border border-gray-200 px-4 py-2">{book.author}</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">ISBN</td>
-            <td className="border border-gray-200 px-4 py-2">{book.ISBN}</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Category
-            </td>
-            <td className="border border-gray-200 px-4 py-2">
-              {book.category}
-            </td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Description
-            </td>
-            <td className="border border-gray-200 px-4 py-2">
-              {book.description}
-            </td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Publication Date
-            </td>
-            <td className="border border-gray-200 px-4 py-2">
-              {book.publication_date}
-            </td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Publisher
-            </td>
-            <td className="border border-gray-200 px-4 py-2">
-              {book.publisher}
-            </td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Language
-            </td>
-            <td className="border border-gray-200 px-4 py-2">
-              {book.language}
-            </td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Page Count
-            </td>
-            <td className="border border-gray-200 px-4 py-2">
-              {book.page_count}
-            </td>
-          </tr>
-          <tr>
-            <td className="border border-gray-200 px-4 py-2 font-bold">
-              Status
-            </td>
-            <td
-              className={`border border-gray-200 px-4 py-2 ${getStatusColor(book.status)}`}
-            >
-              {book.status}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <Link to="/" className="flex items-center gap-2 hover:text-slate-500">
+        <MdArrowBack /> Detail {book.title}
+      </Link>
+      <div className="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <div className="w-full">
+          <img
+            src={book.cover_image}
+            className="mx-auto w-80 rounded-md lg:w-full lg:object-cover"
+            alt={book.title}
+          />
+        </div>
+        <table className="mx-autotable-auto border-collapse border border-gray-200">
+          <tbody>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Title
+              </td>
+              <td className="border border-gray-200 px-4 py-2">{book.title}</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Author
+              </td>
+              <td className="border border-gray-200 px-4 py-2">
+                {book.author}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                ISBN
+              </td>
+              <td className="border border-gray-200 px-4 py-2">{book.ISBN}</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Category
+              </td>
+              <td className="border border-gray-200 px-4 py-2">
+                {book.category}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Description
+              </td>
+              <td className="border border-gray-200 px-4 py-2">
+                {book.description}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Publication Date
+              </td>
+              <td className="border border-gray-200 px-4 py-2">
+                {book.publication_date}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Publisher
+              </td>
+              <td className="border border-gray-200 px-4 py-2">
+                {book.publisher}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Language
+              </td>
+              <td className="border border-gray-200 px-4 py-2">
+                {book.language}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Page Count
+              </td>
+              <td className="border border-gray-200 px-4 py-2">
+                {book.page_count}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-gray-200 px-4 py-2 font-bold">
+                Status
+              </td>
+              <td
+                className={`border border-gray-200 px-4 py-2 ${getStatusColor(book.status)}`}
+              >
+                {book.status}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
